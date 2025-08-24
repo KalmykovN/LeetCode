19cdf99f5137b663cec7aa0f334250803b04af0e
mykola.kalmykov <mykola.kalmykov@adesso.it>
2025-08-24 12:25:15 +0200

    Initial commit

---
+++ src/main/java/com/kalmykov/leetcode/App.java
@@ -0,0 +1,15 @@
+package com.kalmykov.leetcode;
+
+import com.kalmykov.leetcode.yandex.ShortestAirportPath;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ShortestAirportPath solution = new ShortestAirportPath();
+    }
+}
